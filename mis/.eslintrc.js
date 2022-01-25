4045dae4ba52f9e86b7617bd77c1372b88f39f34
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-01-26 05:28:15 +0530

    the module export is no longer needed

--- mis/.eslintrc.js
+++ mis/.eslintrc.js
@@ -1,22 +1,16 @@
-
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-  };
+  },
+};
