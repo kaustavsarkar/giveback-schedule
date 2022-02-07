f3b33a8b1cd45543163fb96a21d10acfb47a2f80
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-02-07 06:21:03 +0530

    Integrating with firebase Google Auth for login

---
+++ mis/src/modules/login/firebase.ts
@@ -0,0 +1,4 @@
+import * as firebase from 'firebase/app';
+import firebaseConfig from 'firebaseConfig';
+
+export const FirebaseApp = firebase.initializeApp(firebaseConfig);
--- mis/src/modules/login/login-card.tsx
+++ mis/src/modules/login/login-card.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './login-card.scss';
 import { ReactComponent as GoogleSvg } from './assets/icons8-google.svg';
+import { googleSignIn } from 'modules/auth/google_auth';
 
-export default function LoginCard() {
+/**
+ * @return {JSX.Element}
+ */
+export default function LoginCard(): JSX.Element {
+  const handleGoogleLogin = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    googleSignIn();
+  };
   return (
     <div className="login-card-container">
       <div className="content-center height-100">
         <div className="content-container">
           <span>Welcome to #giveback</span>
           <ul className="login-icons">
-            <div className="login-svg">
+            <div className="login-svg" onClick={handleGoogleLogin}>
               <GoogleSvg></GoogleSvg>
             </div>
           </ul>
