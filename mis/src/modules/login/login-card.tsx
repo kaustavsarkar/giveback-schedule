1ecad895a22dd2b5222f1ca61b5c2dea2fa86266
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-02-17 10:12:44 +0530

    Updating on click handler for login

--- mis/src/modules/login/login-card.tsx
+++ mis/src/modules/login/login-card.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './login-card.scss';
 import {ReactComponent as GoogleSvg} from './assets/icons8-google.svg';
-import {googleSignIn} from 'modules/auth/google_auth';
 import {useAppSelector, useAppDispatch} from 'state/hooks';
+import {UserProfile} from 'models/user';
+import {loginUser} from 'state/reducers/user';
 
 /**
  * @return {JSX.Element}
  */
 export default function LoginCard(): JSX.Element {
-  const existingUser = useAppSelector((state) => state.user);
+  const existingUser = useAppSelector((state) => state.user) as UserProfile;
   const dispatch = useAppDispatch();
   const handleGoogleLogin = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
-    googleSignIn();
+    if (existingUser?.user?.hasLoggedIn === undefined) {
+      dispatch(loginUser());
+    }
   };
   return (
     <div className="login-card-container">
