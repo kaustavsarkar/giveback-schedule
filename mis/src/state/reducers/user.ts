ac236518135b749aa43f18f862efdb560f93ab37
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-02-17 10:10:35 +0530

    Adding async thunk to the slice along with google sign in method

--- mis/src/state/reducers/user.ts
+++ mis/src/state/reducers/user.ts
@@ -1,38 +1,48 @@
-import {CaseReducer, PayloadAction} from '@reduxjs/toolkit';
+import {CaseReducer, createAsyncThunk, PayloadAction} from '@reduxjs/toolkit';
 import {createSlice} from '@reduxjs/toolkit';
-import {User} from 'models/user';
+import {UserProfile} from 'models/user';
+import {googleSignIn} from 'modules/auth/google_auth';
 import {RootState} from 'state/store';
 
 // Define type of state to be used in reducer.
-type State = User;
+type State = UserProfile;
 
-// Reducer to add user.
-const addUserReducer: CaseReducer<State, PayloadAction<User>> = (
-  state,
-  action,
-) => {
-  state = action.payload;
+const initialState = <UserProfile>{};
+
+export const loginUser = createAsyncThunk('user/login', googleSignIn);
+
+// Reducer for user login.
+const loginReducer: CaseReducer<
+  State,
+  PayloadAction<UserProfile | undefined, string>
+> = (state: State, action: PayloadAction<UserProfile | undefined, string>) => {
+  console.log('inside reducer', state, action);
+  state = action.payload ?? initialState;
+  return state;
 };
 
 // Used for creating action creators and types imlicitly.
 const userSlice = createSlice({
   name: 'user',
-  initialState: <User>{},
+  initialState,
   reducers: {
-    addUser: addUserReducer,
+    // login: loginReducer,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.fulfilled, loginReducer);
   },
 });
 
 // Actions exported from the slice.
 //
 // Actions are to be added as when they are added here.
-export const {addUser} = userSlice.actions;
+// export const {login} = userSlice.actions;
 
 /**
  * The current user present in the state.
  *
  * @param {RootState} state current state of the application.
- * @return {User} User saved in the state.
+ * @return {UserProfile} User saved in the state.
  */
-export const user = (state: RootState) => state.user;
+export const user = (state: RootState): UserProfile => state.user;
 export default userSlice.reducer;
