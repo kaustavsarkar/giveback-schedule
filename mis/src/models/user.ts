cdf3aba7841f693d9d49ec28eba8c690ced1313d
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-02-17 10:04:53 +0530

    Adding GoogleCreds to save login details retrieved via Google OAuth login and UserProfile as a wrapper for User model and other possible authentication mechanisms

--- mis/src/models/user.ts
+++ mis/src/models/user.ts
@@ -1,7 +1,45 @@
+import {IdTokenResult, OAuthCredential, User as FUser} from 'firebase/auth';
+
 // TODO (kaustavsarkar): Make it immutable.
 export interface User {
-  name: string;
-  email: string;
-  displayPicture: string;
-  hasLoggedIn: boolean;
+  readonly name: string;
+  readonly email: string;
+  readonly displayPicture: string;
+  readonly hasLoggedIn: boolean;
+  readonly photoUrl: string;
 }
+
+export interface GoogleCreds {
+  readonly idToken: string;
+  readonly accessToken: string;
+  readonly refreshToken: string;
+  readonly expiresOn: string;
+}
+
+export interface UserProfile {
+  readonly user: User;
+  readonly googleCreds: GoogleCreds;
+}
+
+export const createGoogleUserProfile = (
+  authUser: FUser,
+  oAuthCred: OAuthCredential,
+  idTokenResult?: IdTokenResult,
+): UserProfile => {
+  const user = <User>{
+    email: authUser.email,
+    hasLoggedIn: true,
+    name: authUser.displayName,
+    photoUrl: authUser.photoURL,
+  };
+  const googleCreds = <GoogleCreds>{
+    accessToken: oAuthCred?.accessToken,
+    idToken: oAuthCred?.idToken,
+    expiresOn: idTokenResult?.expirationTime,
+  };
+  const userProfile = <UserProfile>{
+    user: user,
+    googleCreds: googleCreds,
+  };
+  return userProfile;
+};
