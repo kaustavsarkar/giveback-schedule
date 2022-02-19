613c912bea37a723661c44a7208740a313959b80
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-02-20 02:02:26 +0530

    Adding service worker

--- mis/src/index.tsx
+++ mis/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from 'state/store';
-import {Provider} from 'react-redux';
+import * as serviceWorker from 'serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,6 +15,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
