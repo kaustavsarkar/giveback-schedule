5cdc5f64bfcacbe83a586c03c55264826e020f74
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-04-03 02:46:19 +0530

    Adding save button and providing rendering of save and edit buttons based on current state.

--- mis/src/modules/profile/aboutme.tsx
+++ mis/src/modules/profile/aboutme.tsx
@@ -1,17 +1,39 @@
 import './aboutme.scss';
-import React from 'react';
+import React, {useState} from 'react';
 import EditButton from 'shared/edit-button/edit-button';
+import SaveButton from 'shared/save-button/save-button';
 
 export default function AboutMe(props: {aboutMe: string}): JSX.Element {
+  const [isEdit, setEdit] = useState(false);
+
+  const onEdit: React.MouseEventHandler<HTMLDivElement> = () => {
+    setEdit(true);
+  };
+
+  const onSave: React.MouseEventHandler<HTMLDivElement> = () => {
+    setEdit(false);
+  };
+
   return (
     <>
       <div className="profile-about-me">
         <div className="pt-4 border-bottom-1 pb-3">
           <h4 className="text-primary">
             {' '}
-            About Me <EditButton />{' '}
+            About Me{' '}
+            {isEdit ? (
+              <SaveButton onClick={onSave} />
+            ) : (
+              <EditButton onClick={onEdit} />
+            )}{' '}
           </h4>{' '}
-          <p className="mb-2">{props.aboutMe}</p>{' '}
+          {isEdit ? (
+            <textarea autoFocus className="form-control mt-2">
+              {' '}
+            </textarea>
+          ) : (
+            <p className="mb-2">{props.aboutMe}</p>
+          )}
         </div>{' '}
       </div>{' '}
     </>
--- mis/src/shared/save-button/save-button.tsx
+++ mis/src/shared/save-button/save-button.tsx
@@ -1,16 +1,16 @@
-import './edit-button.scss';
+import './save-button.scss';
 import React from 'react';
 
-export default function EditButton(props: {
+export default function SaveButton(props: {
   onClick?: React.MouseEventHandler<HTMLDivElement>;
 }) {
   return (
     <div
       onClick={props.onClick}
-      className="btn btn-outline-primary rounded ml-2 btn-sm px-4"
+      className="btn btn-solid-primary rounded ml-2 btn-sm px-4"
     >
       {' '}
-      Edit{' '}
+      Save{' '}
     </div>
   );
 }
