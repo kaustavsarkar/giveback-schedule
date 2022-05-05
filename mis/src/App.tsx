e61a005e83e10d529021b5ec5b0d094ecdeff3ca
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-05-06 03:42:19 +0530

    adding routes to the components

--- mis/src/App.tsx
+++ mis/src/App.tsx
@@ -5,6 +5,8 @@ import Home from 'modules/home/home';
 import Login from 'modules/login/login';
 import {PrivateRoute} from 'private_route';
 import ProfilePage from 'modules/profile/profile';
+import Schedules from 'modules/schedules/schedules';
+import CreateSchedule from 'modules/schedules/create/create-schedule';
 
 /**
  *
@@ -18,7 +20,8 @@ function App(): JSX.Element {
         <Route path="/login" element={<Login />} />
         <Route element={<PrivateRoute redirectTo="/login" />}>
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/dashboard" element={<></>}></Route>
+          <Route path="/schedules" element={<Schedules />}></Route>
+          <Route path="/schedules/create" element={<CreateSchedule />}></Route>
         </Route>
       </Routes>
     </Router>
