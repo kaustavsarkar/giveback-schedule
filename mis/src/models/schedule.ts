11a64a92ed609b6d2d3e5ad2b48ceec2becfc7f0
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2022-09-26 02:59:59 +0530

    Updating schedule and interview models and adding feedback models

--- mis/src/models/schedule.ts
+++ mis/src/models/schedule.ts
@@ -1,3 +1,4 @@
+import {upsertInterview} from 'services/schedule-service';
 import {Skills} from './skills';
 import {User} from './user';
 
@@ -13,6 +14,8 @@ export interface Schedule {
   readonly interviewee?: User;
   readonly skills: Skills;
   readonly status?: Status;
+  readonly googleMeetLink?: string;
+  readonly googleDocLink?: string;
 }
 
 export class Interview implements Schedule {
@@ -24,6 +27,16 @@ export class Interview implements Schedule {
   interviewee?: User | undefined;
   skills: Skills;
   status?: Status | undefined;
+  googleMeetLink?: string;
+  googleDocLink?: string;
+  /**
+   * Feedback given by the interviewer to the interviewee.
+   */
+  intervieweeFeedback?: IntervieweeFeedback;
+  /**
+   * Feedback given by the interviewee to the interviewer.
+   */
+  interviewerFeedback?: InterviewerFeedback;
 
   // This is the currently logged in user.
   currentUser: User;
@@ -37,7 +50,11 @@ export class Interview implements Schedule {
     this.interviewee = interview.interviewee;
     this.skills = interview.skills;
     this.status = interview.status;
-    this.currentUser = currentUser;
+    this.currentUser = <User>{
+      email: currentUser.email,
+    };
+    this.googleMeetLink = interview.googleMeetLink;
+    this.googleDocLink = interview.googleDocLink;
   }
 
   /**
@@ -125,6 +142,134 @@ export class Interview implements Schedule {
     // If the interview is to happen now.
     return 'Click here to start the Interview';
   };
+
+  /**
+   * To be called when the interview is done and final feedback is
+   * being provided by the interviewer and the interviewee.
+   */
+  save = async (): Promise<void> => {
+    this.status = 'DONE';
+    await upsertInterview(this);
+  };
+
+  hasIntervieweeFeedback = (): boolean =>
+    this.intervieweeFeedback !== undefined &&
+    this.intervieweeFeedback !== null &&
+    this.intervieweeFeedback.writtenFeedback !== undefined &&
+    this.intervieweeFeedback.writtenFeedback !== null &&
+    this.intervieweeFeedback.writtenFeedback.length > 0;
+
+  hasInterviewerFeedback = (): boolean =>
+    this.interviewerFeedback !== undefined &&
+    this.interviewerFeedback !== null &&
+    this.interviewerFeedback.comments !== undefined &&
+    this.interviewerFeedback.comments !== null &&
+    this.interviewerFeedback.comments.length > 0;
+
+  showProvideFeedbackSection = (): boolean => {
+    if (!this.isPastInterview()) {
+      return true;
+    }
+
+    console.log(this.interviewerFeedback);
+
+    if (this.isInterviewee() && !this.hasInterviewerFeedback()) {
+      return true;
+    }
+
+    if (this.isInterviewer() && !this.hasIntervieweeFeedback()) {
+      return true;
+    }
+
+    return false;
+  };
+}
+
+export class IntervieweeFeedback {
+  private _writtenFeedback?: string | undefined;
+  private _implementation?: number | undefined = 1;
+  private _communication?: number | undefined = 1;
+  private _hireProbability?: number | undefined = 1;
+  private _approach?: number | undefined = 1;
+
+  public get writtenFeedback(): string | undefined {
+    return this._writtenFeedback;
+  }
+  public set writtenFeedback(value: string | undefined) {
+    this._writtenFeedback = value;
+  }
+  public get approach(): number | undefined {
+    return this._approach;
+  }
+  public set approach(value: number | undefined) {
+    this._approach = value;
+  }
+  public get implementation(): number | undefined {
+    return this._implementation;
+  }
+  public set implementation(value: number | undefined) {
+    this._implementation = value;
+  }
+  public get communication(): number | undefined {
+    return this._communication;
+  }
+  public set communication(value: number | undefined) {
+    this._communication = value;
+  }
+  private _hintUtilisation?: number | undefined = 1;
+  public get hintUtilisation(): number | undefined {
+    return this._hintUtilisation;
+  }
+  public set hintUtilisation(value: number | undefined) {
+    this._hintUtilisation = value;
+  }
+  public get hireProbability(): number | undefined {
+    return this._hireProbability;
+  }
+  public set hireProbability(value: number | undefined) {
+    this._hireProbability = value;
+  }
+}
+
+export class InterviewerFeedback {
+  private _comments?: string | undefined;
+  private _respectful?: number | undefined = 1;
+  private _helpful?: number | undefined = 1;
+  private _clarity?: number | undefined = 1;
+  private _preparedness?: number | undefined = 1;
+
+  public get comments(): string | undefined {
+    return this._comments;
+  }
+  public set comments(value: string | undefined) {
+    this._comments = value;
+  }
+
+  public get respectful(): number | undefined {
+    return this._respectful;
+  }
+  public set respectful(value: number | undefined) {
+    this._respectful = value;
+  }
+  public get helpful(): number | undefined {
+    return this._helpful;
+  }
+  public set helpful(value: number | undefined) {
+    this._helpful = value;
+  }
+  public get clarity(): number | undefined {
+    return this._clarity;
+  }
+  public set clarity(value: number | undefined) {
+    this._clarity = value;
+  }
+
+  public get preparedness(): number | undefined {
+    return this._preparedness;
+  }
+  public set preparedness(value: number | undefined) {
+    this._preparedness = value;
+  }
 }
 
 type Status = 'AVAILABLE' | 'DONE' | 'BOOKED';
